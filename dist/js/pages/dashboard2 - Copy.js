4a649aac033feb24e86d2d4532c69b517b406b4a
JC <jashwant.chouhan@gmail.com>
2019-12-25 17:59:35 +0530

    updated managers dashboard OneToOneService/iService-Core/issues/1522

--- dist/js/pages/dashboard2 - Copy.js
+++ dist/js/pages/dashboard2 - Copy.js
@@ -17,10 +17,10 @@ $(function () {
   var salesChart       = new Chart(salesChartCanvas);
 
   var salesChartData = {
-    labels  : ['1 Dec', '2 Dec', '3 Dec', '4 Dec', '5 Dec', '6 Dec', '7 Dec'],
+    labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [
       {
-        label               : 'Question',
+        label               : 'Electronics',
         fillColor           : 'rgb(210, 214, 222)',
         strokeColor         : 'rgb(210, 214, 222)',
         pointColor          : 'rgb(210, 214, 222)',
@@ -30,14 +30,14 @@ $(function () {
         data                : [65, 59, 80, 81, 56, 55, 40]
       },
       {
-        label               : 'Answer',
+        label               : 'Digital Goods',
         fillColor           : 'rgba(60,141,188,0.9)',
         strokeColor         : 'rgba(60,141,188,0.8)',
         pointColor          : '#3b8bba',
         pointStrokeColor    : 'rgba(60,141,188,1)',
         pointHighlightFill  : '#fff',
         pointHighlightStroke: 'rgba(60,141,188,1)',
-        data                : [28, 48, 70, 75, 45, 40, 30]
+        data                : [28, 48, 40, 19, 86, 27, 90]
       }
     ]
   };
@@ -96,16 +96,40 @@ $(function () {
   var pieChart       = new Chart(pieChartCanvas);
   var PieData        = [
     {
-      value    : 5,
+      value    : 700,
       color    : '#f56954',
       highlight: '#f56954',
-      label    : 'Logged Out Agents'
+      label    : 'Chrome'
     },
     {
-      value    : 15,
+      value    : 500,
+      color    : '#00a65a',
+      highlight: '#00a65a',
+      label    : 'IE'
+    },
+    {
+      value    : 400,
+      color    : '#f39c12',
+      highlight: '#f39c12',
+      label    : 'FireFox'
+    },
+    {
+      value    : 600,
       color    : '#00c0ef',
       highlight: '#00c0ef',
-      label    : 'Logged In Agents'
+      label    : 'Safari'
+    },
+    {
+      value    : 300,
+      color    : '#3c8dbc',
+      highlight: '#3c8dbc',
+      label    : 'Opera'
+    },
+    {
+      value    : 100,
+      color    : '#d2d6de',
+      highlight: '#d2d6de',
+      label    : 'Navigator'
     }
   ];
   var pieOptions     = {
@@ -132,67 +156,13 @@ $(function () {
     // String - A legend template
     legendTemplate       : '<ul class=\'<%=name.toLowerCase()%>-legend\'><% for (var i=0; i<segments.length; i++){%><li><span style=\'background-color:<%=segments[i].fillColor%>\'></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>',
     // String - A tooltip template
-    tooltipTemplate      : '<%=value %> <%=label%>'
+    tooltipTemplate      : '<%=value %> <%=label%> users'
   };
   // Create pie or douhnut chart
   // You can switch between pie and douhnut using the method below.
   pieChart.Doughnut(PieData, pieOptions);
   // -----------------
   // - END PIE CHART -
-  // -----------------
-
-
-  // -------------
-  // - PIE CHART 2-
-  // -------------
-  // Get context with jQuery - using jQuery's .get() method.
-  var pieChartCanvas = $('#pieChart2').get(0).getContext('2d');
-  var pieChart       = new Chart(pieChartCanvas);
-  var PieData        = [
-    {
-      value    : 15,
-      color    : '#00a659',
-      highlight: '#00a659',
-      label    : 'Logged In'
-    },
-    {
-      value    : 3,
-      color    : '#f39c11',
-      highlight: '#f39c11',
-      label    : 'Logged Out'
-    }
-  ];
-  var pieOptions     = {
-    // Boolean - Whether we should show a stroke on each segment
-    segmentShowStroke    : true,
-    // String - The colour of each segment stroke
-    segmentStrokeColor   : '#fff',
-    // Number - The width of each segment stroke
-    segmentStrokeWidth   : 1,
-    // Number - The percentage of the chart that we cut out of the middle
-    percentageInnerCutout: 50, // This is 0 for Pie charts
-    // Number - Amount of animation steps
-    animationSteps       : 100,
-    // String - Animation easing effect
-    animationEasing      : 'easeOutBounce',
-    // Boolean - Whether we animate the rotation of the Doughnut
-    animateRotate        : true,
-    // Boolean - Whether we animate scaling the Doughnut from the centre
-    animateScale         : false,
-    // Boolean - whether to make the chart responsive to window resizing
-    responsive           : true,
-    // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-    maintainAspectRatio  : false,
-    // String - A legend template
-    legendTemplate       : '<ul class=\'<%=name.toLowerCase()%>-legend\'><% for (var i=0; i<segments.length; i++){%><li><span style=\'background-color:<%=segments[i].fillColor%>\'></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>',
-    // String - A tooltip template
-    tooltipTemplate      : '<%=value %> <%=label%>'
-  };
-  // Create pie or douhnut chart
-  // You can switch between pie and douhnut using the method below.
-  pieChart.Doughnut(PieData, pieOptions);
-  // -----------------
-  // - END PIE CHART 2-
   // -----------------
 
   /* jVector Maps
