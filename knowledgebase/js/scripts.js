adb04ea3b464c6310026a1547f77c017e66c9320
jaswantgit <vedantschoolpodar@gmail.com>
2021-05-31 14:53:04 +0530

    updated article kb article detail page changes
    updated article kb article detail page changes 3250

--- knowledgebase/js/scripts.js
+++ knowledgebase/js/scripts.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['ui.bootstrap','iService.directive','ngAnimate','ui.select','datatables', 'datatables.bootstrap', 'datatables.buttons']);
+var app = angular.module('myApp', ['ui.bootstrap','iService.directive','ngAnimate','ui.select','datatables', 'datatables.bootstrap', 'datatables.buttons','angular-star-rating']);
 var tabName = '';
  app.controller('knowledgebaseCtrl', function ($scope, $window,$uibModal, DTOptionsBuilder, DTColumnDefBuilder) {
     $scope.tabtype = 'home';
@@ -128,9 +128,14 @@ var tabName = '';
 
 		}
 	}
+
+	$scope.isReadonly = false; // default test value
+    $scope.changeOnHover = false; // default test value 
+    $scope.maxValue = 5; // default test value
+    $scope.ratingValue = 0; // default test value
 			
 });
-
+angular.module('angular-star-rating', []).directive('angularStarRating', angularStarRating);
 app.controller('TopicCtrl', function ($scope, $http, $timeout, $interval) {
 var vm = this;
 vm.topicselected = {selected : "Select or search for a topic in the list ..."};
@@ -156,5 +161,72 @@ vm.topicselected = {selected : "Select or search for a topic in the list ..."};
 ];
 
 });
+function angularStarRating() {
+    var directive = {
 
+        restrict: 'EA',
+        scope: {
+            'value': '=value',
+                'max': '=max',
+                'hover': '=hover',
+                'isReadonly': '=isReadonly'
+        },
+        link: linkFunc,
+        template:
+            '<span ng-class="{isReadonly: isReadonly}">' +
+            '<i ng-class="renderObj" ' +
+            'ng-repeat="renderObj in renderAry" ' +
+            'ng-click="setValue($index)" ' +
+            'ng-mouseenter="changeValue($index, changeOnHover )" >' +
+            '</i>' +
+            '</span>',
+        replace: true
+    };
+    return directive;
+}
 
+function linkFunc(scope, element, attrs, ctrl) {
+    if (scope.max === undefined) scope.max = 5; // default
+    console.log(scope.test);
+
+    function renderValue() {
+        scope.renderAry = [];
+        for (var i = 0; i < scope.max; i++) {
+            if (i < scope.value) {
+                scope.renderAry.push({
+                    'fa fa-star fa-2x': true
+                });
+            } else {
+                scope.renderAry.push({
+                    'fa fa-star-o fa-2x': true
+                });
+            }
+        }
+    }
+
+    scope.setValue = function (index) {
+        if (!scope.isReadonly && scope.isReadonly !== undefined) {
+            scope.value = index + 1;
+        }
+    };
+
+    scope.changeValue = function (index) {
+        if (scope.hover) {
+            scope.setValue(index);
+        } else {
+            // !scope.changeOnhover && scope.changeOnhover != undefined
+        }
+    };
+
+    scope.$watch('value', function (newValue, oldValue) {
+        if (newValue) {
+            renderValue();
+        }
+    });
+    scope.$watch('max', function (newValue, oldValue) {
+        if (newValue) {
+            renderValue();
+        }
+    });
+
+}
